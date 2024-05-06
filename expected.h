a09260b263223f93a2e9960cc2be4a03f127dd78
Nguyen Thanh An <thanhan030301@gmail.com>
2024-05-06 21:28:19 +0700

    add simple lexer

---
+++ expected.h
@@ -0,0 +1,184 @@
+#ifndef QUERY_LANG_EXPECTED_H
+#define QUERY_LANG_EXPECTED_H
+
+#include <concepts>
+#include <type_traits>
+#include <variant>
+
+
+struct ExpectTag_t {
+};
+
+static constexpr ExpectTag_t ExpectTag{};
+
+struct UnexpectTag_t {
+};
+
+static constexpr UnexpectTag_t UnexpectTag{};
+
+template <typename E>
+class Unexpected {
+public:
+    explicit Unexpected(E const &e)
+        : err(e) {
+    }
+
+    explicit Unexpected(E &&e)
+        : err(std::move(e)) {
+    }
+
+    auto error() & -> E & { return err; }
+    auto error() const & -> E const & { return err; }
+    auto error() && -> E && { return std::move(err); }
+    auto error() const && -> E const && { return std::move(err); }
+
+    constexpr auto swap(
+        Unexpected &other) noexcept(std::is_nothrow_swappable_v<E>)
+        requires std::is_swappable_v<E> {
+        return std::swap(this->err, other.err);
+    }
+
+private:
+    E err;
+};
+
+template <typename T, typename E>
+    requires std::is_destructible_v<T> &&
+             (!std::convertible_to<T, Unexpected<E>>)
+class Expected {
+public:
+    using value_type = T;
+    using error_type = E;
+    using unexpected_type = Unexpected<E>;
+
+    constexpr Expected(const Expected &other) = default;
+
+    constexpr explicit Expected(value_type &&value)
+        : _variant(std::move(value)) {
+    }
+
+    constexpr explicit Expected(error_type &&e)
+        : _variant(unexpected_type{std::move(e)}) {
+    }
+
+    template <typename U>
+    constexpr Expected(ExpectTag_t, U const &value)
+        : _variant(value_type{value}) {
+    }
+
+    template <typename U>
+    constexpr Expected(UnexpectTag_t, U const &err)
+        : _variant(unexpected_type{err}) {
+    }
+
+    [[nodiscard]]
+
+    constexpr auto has_value() const noexcept -> bool {
+        return std::holds_alternative<value_type>(_variant);
+    }
+
+    [[nodiscard]] constexpr auto has_error() const noexcept -> bool {
+        return not has_value();
+    }
+
+    [[nodiscard]] constexpr auto value() & -> value_type & {
+        return std::get<value_type>(_variant);
+    }
+
+    [[nodiscard]] constexpr auto value() const & -> value_type const & {
+        return std::get<value_type>(_variant);
+    }
+
+    [[nodiscard]] constexpr auto value() && -> value_type && {
+        return std::get<value_type>(std::move(_variant));
+    }
+
+    [[nodiscard]] constexpr auto value() const && -> value_type const && {
+        return std::get<value_type>(std::move(_variant));
+    }
+
+    [[nodiscard]] constexpr auto error() & -> error_type & {
+        return std::get<unexpected_type>(_variant).error();
+    }
+
+    [[nodiscard]] constexpr auto error() const & -> error_type const & {
+        return std::get<unexpected_type>(_variant).error();
+    }
+
+    [[nodiscard]] constexpr auto error() && -> error_type && {
+        return std::get<unexpected_type>(std::move(_variant)).error();
+    }
+
+    [[nodiscard]] constexpr auto error() const && -> error_type const && {
+        return std::get<unexpected_type>(std::move(_variant)).error();
+    }
+
+    template <typename U>
+        requires std::convertible_to<U &&, T> && std::move_constructible<T>
+    [[nodiscard]] constexpr auto value_or(U &&default_value) && -> T {
+        if (has_value()) {
+            return std::move(this->value());
+        }
+        return static_cast<T>(std::forward<U>(default_value));
+    }
+
+    template <typename U>
+        requires std::convertible_to<U &&, T> && std::copy_constructible<T>
+    [[nodiscard]] constexpr auto value_or(U &&default_value) const & -> T {
+        if (has_value()) {
+            return value();
+        }
+        return static_cast<T>(std::forward<U>(default_value));
+    }
+
+    template <typename Fn>
+        requires std::is_invocable_v<Fn, T &>
+    [[nodiscard]] constexpr auto transform(Fn &&fn) & {
+        if (has_value()) {
+            return Expected<std::remove_cv_t<std::invoke_result_t<Fn>>, E>{
+                ExpectTag, fn(value())
+            };
+        }
+        return Expected{UnexpectTag, error()};
+    }
+
+    template <typename Fn>
+        requires std::is_invocable_v<Fn, T const &>
+    [[nodiscard]] constexpr auto transform(Fn &&fn) const & {
+        if (has_value()) {
+            return Expected<std::remove_cv_t<std::invoke_result_t<Fn>>, E>{
+                ExpectTag, fn(value())
+            };
+        }
+        return Expected{UnexpectTag, error()};
+    }
+
+    template <typename Fn>
+        requires std::is_invocable_v<Fn, T &&>
+    [[nodiscard]] constexpr auto transform(Fn &&fn) && {
+        if (has_value()) {
+            return Expected<std::remove_cv_t<std::invoke_result_t<Fn>>, E>{
+                ExpectTag, fn(std::move(value()))
+            };
+        }
+        return Expected{UnexpectTag, error()};
+    }
+
+    template <typename Fn>
+        requires std::is_invocable_v<Fn, T const &&>
+    [[nodiscard]] constexpr auto transform(Fn &&fn) && {
+        if (has_value()) {
+            return Expected<std::remove_cv_t<std::invoke_result_t<Fn>>, E>{
+                ExpectTag, fn(std::move(value()))
+            };
+        }
+        return Expected{UnexpectTag, error()};
+    }
+
+private:
+    constexpr Expected() = default;
+
+    std::variant<value_type, unexpected_type> _variant;
+};
+
+#endif //QUERY_LANG_EXPECTED_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,213 @@
+#include "expected.h"
+
 #include <iostream>
+#include <vector>
+#include <string>
+#include <format>
+#include <iomanip>
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+enum class TokenType {
+    And,
+    Or,
+    True,
+    False,
+
+    LParen,
+    RParen,
+};
+
+struct SourcePosition {
+    ssize_t line;
+    ssize_t colum;
+};
+
+std::ostream &operator<<(std::ostream &o, SourcePosition const &p) {
+    o << '[' << p.line << ':' << p.colum << ']';
+    return o;
+}
+
+struct SourceRange {
+    SourcePosition start;
+    SourcePosition end;
+};
+
+std::ostream &operator<<(std::ostream &o, SourceRange const &p) {
+    o << '[' << p.start << '-' << p.end << ']';
+    return o;
+}
+
+struct Token {
+    TokenType type;
+    std::string_view literal;
+    SourceRange source_range;
+};
+
+std::ostream &operator<<(std::ostream &o, Token const &tk) {
+    o << '{' << static_cast<int>(tk.type) << " | " <<
+        tk.source_range << " | " <<
+        std::quoted(tk.literal) << '}';
+    return o;
+}
+
+enum class ParseErrorType {
+    Eof,
+    UnknownToken,
+};
+
+struct ParseError {
+    ParseErrorType type;
+    SourceRange source_range;
+    std::string cause;
+};
+
+class Lexer {
+public:
+    using ExpToken = Expected<Token, ParseError>;
+
+    explicit Lexer(std::string const &source_)
+        : source(source_),
+          current_idx(0),
+          current_position({}) {
+    }
+
+    auto lex() -> Expected<std::vector<Token>, ParseError> {
+        using result_t = Expected<std::vector<Token>, ParseError>;
+        auto tokens = std::vector<Token>{};
+
+        while (not is_at_end()) {
+            auto const c = get_current();
+            auto tk = Token{
+                .literal = std::string_view{source}.substr(current_idx, 1),
+                .source_range = SourceRange{current_position, current_position},
+            };
+
+            switch (c) {
+            case ' ':
+            case '\t':
+                advance();
+                break;
+            case '(':
+                tk.type = TokenType::LParen;
+                tokens.push_back(tk);
+                advance();
+                break;
+            case ')':
+                tk.type = TokenType::RParen;
+                tokens.push_back(tk);
+                advance();
+                break;
+            default: {
+                auto const tk_exp = keyword_or_identifier();
+                if (tk_exp.has_error()) {
+                    return result_t{UnexpectTag, tk_exp.error()};
+                }
+                tokens.push_back(tk_exp.value());
+            }
+            break;
+            }
+        }
+
+        return result_t{ExpectTag, tokens};
+    }
+
+private:
+    auto keyword_or_identifier() -> ExpToken {
+        auto const start = current_position;
+        auto const start_idx = current_idx;
+        while (not is_at_end() and std::isalpha(get_current())) {
+            advance();
+        }
+        auto const source_range = SourceRange{start, current_position};
+
+        // check for key worlds
+        auto const raw = std::string_view{source}.
+            substr(start_idx, current_idx - start_idx);
+        auto tk = std::optional<TokenType>();
+        if (raw == "and"sv) {
+            tk = std::make_optional(TokenType::And);
+        } else if (raw == "or"sv) {
+            tk = std::make_optional(TokenType::Or);
+        } else if (raw == "true"sv) {
+            tk = std::make_optional(TokenType::True);
+        } else if (raw == "false"sv) {
+            tk = std::make_optional(TokenType::False);
+        }
+        if (!tk.has_value()) {
+            return ExpToken{
+                ParseError{
+                    .type = ParseErrorType::UnknownToken,
+                    .source_range = source_range,
+                    .cause = std::format("unknown literal `{}`", raw),
+                }
+            };
+        }
+
+        return ExpToken{
+            Token{
+                .type = tk.value(),
+                .literal = raw,
+                .source_range = source_range,
+            }
+        };
+    }
+
+    [[nodiscard]] auto is_at_end() const -> bool {
+        return current_idx >= source.size();
+    }
+
+    [[nodiscard]] auto get_current() const -> char {
+        if (is_at_end()) {
+            return 0;
+        }
+        return source[current_idx];
+    }
+
+    [[nodiscard]] auto get_peek() const -> char {
+        if (current_idx + 1 >= source.size()) {
+            return 0;
+        }
+        return source[current_idx + 1];
+    }
+
+    auto advance() -> char {
+        if (is_at_end()) {
+            return 0;
+        }
+
+        const auto old_idx = current_idx;
+        current_idx += 1;
+        current_position.colum += 1;
+
+        if (last_value == '\n') {
+            current_position.line += 1;
+            current_position.colum = 0;
+        }
+
+        last_value = source[old_idx];
+        return last_value;
+    }
+
+    std::string const &source;
+    size_t current_idx;
+    SourcePosition current_position;
+    char last_value = 0;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    auto source = "true and false or (true or((())) true false )"s;
+
+    auto lexer = Lexer(source);
+    auto const tokens = lexer.lex();
+
+    if (tokens.has_error()) {
+        std::cout << tokens.error().cause;
+        return 0;
+    }
+
+    for (auto const &tk : tokens.value()) {
+        std::cout << tk << '\n';
+    }
 }
